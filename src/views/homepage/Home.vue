043a6cbc905be224c065edcb7a7971f04906cacb
qinhualian <2577986066@qq.com>
2023-05-23 16:22:04 +0800

    进度

--- src/views/homepage/Home.vue
+++ src/views/homepage/Home.vue
@@ -1,15 +1,27 @@
 <template>
   <div class="home-page">
-    <el-container>
+    <!-- <el-container>
       <HeadPage></HeadPage>
       <el-container>
         <HeadMain></HeadMain>
 
         <el-footer>Footer</el-footer>
       </el-container>
-    </el-container>
+    </el-container> -->
   </div>
 </template>
 
-<script lang="ts" src="./home.ts"></script>
+<!-- <script lang="ts" src="./home.ts">
+
+</script> -->
+<script>
+  let count = 0
+  const nums = [0, 1, 2, 3]
+
+  nums.forEach((num) => {
+    if (num) count += num
+  })
+
+  console.log(count)
+</script>
 <style lang="less" src="./home.less"></style>
